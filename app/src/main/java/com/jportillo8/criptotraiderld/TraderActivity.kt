d9b2c608e46a0bcfd855cfd6588d76b199ede434
jportillo8 <buttokk@gmail.com>
2020-03-04 23:11:38 -0500

    Base del Curso, Primer Commit

---
+++ app/src/main/java/com/jportillo8/criptotraiderld/TraderActivity.kt
@@ -0,0 +1,18 @@
+package com.jportillo8.criptotraiderld
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+/**
+ * @author Santiago Carrillo
+ * 2/14/19.
+ */
+class TraderActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trader)
+    }
+}
