39de1c7d7d38265e7a8ec8f80cb375ddc7b7c49e
jportillo8 <buttokk@gmail.com>
2020-03-10 15:50:06 -0500

    "Implementado mensajes del Bottom en crypto trider activity"

--- app/src/main/java/com/jportillo8/criptotraiderld/TraderActivity.kt
+++ app/src/main/java/com/jportillo8/criptotraiderld/TraderActivity.kt
@@ -1,7 +1,10 @@
 package com.jportillo8.criptotraiderld
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_trader.*
 
 
 /**
@@ -14,5 +17,14 @@ class TraderActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_trader)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "generating new cryptos", Snackbar.LENGTH_SHORT )
+                .setAction("Info", null).show()
+        }
+    }
+    fun showGeneralServerErrorMessage(){
+        Snackbar.make(fab, "Error while connecting to the server", Snackbar.LENGTH_SHORT)
+            .setAction("Info",null).show()
     }
 }
